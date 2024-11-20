226b343cbe62aa99246417ce82062636a13d0433
BennyButNot <57555952+ReeceSpencer@users.noreply.github.com>
2024-11-20 22:36:24 +0800

    add python run

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+import settings
 import discord
 import random
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+    bot = commands.Bot(command_prefix='$', intents=intents)
 
-@bot.hybrid_command(name='ping')
-async def ping(context):
-    await context.send('pong')
+    @bot.hybrid_command(name='ping')
+    async def ping(context):
+        await context.send('pong')
 
-@bot.hybrid_command(name='dice')
-async def dice(context, arg=6):
-    await context.send(random.randint(1, arg))
+    @bot.hybrid_command(name='dice')
+    async def dice(context, arg=6):
+        await context.send(random.randint(1, arg))
 
-#bot.run(DISCORD_API_TOKEN)
+    bot.run(settings.DISCORD_API_TOKEN)
+
+if __name__ == "__main__":
+    run()
